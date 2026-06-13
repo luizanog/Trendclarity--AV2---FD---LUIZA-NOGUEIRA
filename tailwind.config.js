19c10ddbe85e46e3bb580ef2327568ae931dd7c8
LUIZA NOGUEIRA DO NASCIMENTO <151073438+luizanog@users.noreply.github.com>
2026-06-13 01:38:36 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-content: ['./index.html', './*.{js,jsx}'],
+  content: ['./index.html', './*.{js,jsx}'],
   theme: {
     extend: {
       colors: {
-        brand: {
-          50:  '#ede9ff',
-          100: '#d4caff',
-          200: '#b3a4ff',
-          400: '#9b77ff',
-          500: '#7c5cfc',
-          600: '#6c47ff',
-          800: '#3b1d8a',
-        },
+        brand: { 50:'#ede9ff', 100:'#d4caff', 200:'#b3a4ff', 400:'#9b77ff', 500:'#7c5cfc', 600:'#6c47ff', 800:'#3b1d8a' },
         coral: '#ff6b6b',
         peach: '#ffb07a',
         yellow: '#ffd166',
